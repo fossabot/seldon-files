ee75106d2a5c19b3c5d30c010bf4ad0396c0d3fd
Akshat Sinha <akshat.s@media.net>
2020-06-26 16:57:43 +0530

    Build using script -- 5

--- build.sh
+++ build.sh
@@ -7,15 +7,12 @@ echo $PROJECT
 echo $BRANCH
 echo $NAME
 echo $VERSION
-if [[$BRANCH == 'develop']]
-then
+if [[ $BRANCH == 'develop' ]]; then
     echo "Building Nightly version"
     docker build . -t gcr.io/granular-ai/$NAME:$SHORT_SHA -t gcr.io/granular-ai/$NAME:nightly --cache-from gcr.io/granular-ai/$NAME:nightly 
-done
 else
     echo "Building Stable version"
     docker build . -t gcr.io/granular-ai/$NAME:$SHORT_SHA -t gcr.io/granular-ai/$NAME:latest -t gcr.io/granular-ai/$NAME:stable  -t gcr.io/granular-ai/$NAME:$VERSION --cache-from gcr.io/granular-ai/$NAME:nightly 
-done
 fi
     
 docker push gcr.io/granular-ai/$NAME
