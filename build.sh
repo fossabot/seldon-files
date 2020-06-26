e81eb0d0de75a71fff825b394ac89804bb071bb7
Akshat Sinha <akshat.s@media.net>
2020-06-26 16:41:55 +0530

    Build using script -- 2

--- build.sh
+++ build.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
-echo "Building Seldon Image from $BRANCH_NAME"
-echo "Repo Name $REPO_NAME"
-# if [[ "$BRANCH_NAME" == "develop" ]]
-# then
-# docker build . -t gcr.io/granular-ai/$REPO_NAME:$SHORT_SHA -t gcr.io/granular-ai/$REPO_NAME:latest --cache-from gcr.io/granular-ai/$REPO_NAME:latest"
+echo "Hello from Bash"
+VERSION=`cat VERSION`
+echo $BUILD
+echo $PROJECT
+echo $BRANCH
+echo $NAME
+echo $VERSION
+if [[$BRANCH == 'develop']]
+then
+    echo "Building Nightly version"
+    docker build -t gcr.io/granular-ai/$NAME:$SHORT_SHA -t gcr.io/granular-ai/$NAME:nightly --cache-from gcr.io/granular-ai/$NAME:nightly 
+done
+else
+    echo "Building Stable version"
+    docker build -t gcr.io/granular-ai/$NAME:$SHORT_SHA -t gcr.io/granular-ai/$NAME:latest -t gcr.io/granular-ai/$NAME:stable  -t gcr.io/granular-ai/$NAME:$VERSION --cache-from gcr.io/granular-ai/$NAME:nightly 
+done
+fi
+    
+docker push gcr.io/granular-ai/$NAME
